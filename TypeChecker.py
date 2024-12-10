2a1d2fadcb94240a8cec6f149b6abc3805b1e8a6
Jan Dabrowski <jandabrowski@heineken.com>
2024-12-10 11:07:14 +0100

    matrix assignment works

--- TypeChecker.py
+++ TypeChecker.py
@@ -68,70 +68,15 @@ class TypeChecker(NodeVisitor):
 
     def new_error(self, line, message):
         self.errors.append(f"Error at line {line}: {message}")
+        self.print_errors()
 
     def visit_InstructionsNode(self, node):
-        try:
-            for instruction in node.instructions:
+        for instruction in node.instructions:
                 self.visit(instruction)
-        except:
-            print("Tu sie wywala: ", instruction, node.lineno)
-
-    # def visit_AssignExpression(self, node):
-    #     type = self.visit(node.right)
-
-    #     if (isinstance(node.left, AST.MatrixRefNode)):
-    #         return  # to jest coś typu x[1] = 0;
-
-    #     var_name = node.left.value
-    #     var_type = "var"
-    #     size = None
-    #     row_sizes = []
-    #     # z jakiegoś powodu MatrixNode nie jest z ExpressionNode a reszta tak
-    #     if (isinstance(node.right, AST.MatrixNode)):
-    #         var_type = "matrix"
-    #         size = len(node.right.values.values)
-    #         for row in node.right.values.values:
-    #             if (isinstance(row, AST.StringOfNumNode)):
-    #                 row_sizes.append(len(row.values))
-
-    #     elif (isinstance(node.right, AST.BinExpr)):
-    #         var_type = type
-
-    #     elif (isinstance(node.right.expr, AST.IntNum)):
-    #         var_type = "int"
-    #     elif (isinstance(node.right.expr, AST.FloatNum)):
-    #         var_type = "float"
-    #     elif (isinstance(node.right.expr, AST.StringNode)):
-    #         var_type = "string"
-    #     elif (isinstance(node.right.expr, AST.MatrixFuncNode)):
-    #         var_type = "matrix"
-    #         size = node.right.expr.arg
-    #         for row in range(size):
-    #             row_sizes.append(size)
-
-    #     # MATRIX INITIALIZATION CHECK
-    #     if (isinstance(node.right, AST.MatrixNode)):
-    #         row_len = -1
-    #         if (isinstance(node.right.values, AST.MatrixRowsNode)):
-    #             for row in node.right.values.values:  # xd2
-    #                 if (row_len == -1):
-    #                     row_len = len(row.values)
-    #                 elif (len(row.values) != row_len):
-    #                     self.new_error(
-    #                         node.lineno, "Incorrect size of matrix rows!")
-    #                     return
-    #     # -----------------------------
-
-    #     var = VariableSymbol(var_name, var_type, size, row_sizes)
-
-    #     if var_type != "":
-    #         self.current_scope.put(var_name, var)
-    #         self.visit(node.left)
 
     def visit_AssignExpression(self, node):
         var_type = None
         size = None
-        row_sizes = []
 
         type = self.visit(node.right)
 
@@ -139,40 +84,10 @@ class TypeChecker(NodeVisitor):
             return
 
         var_name = node.left.name
+        var_type = type
 
         if type == 'matrix':
-            var_type = 'matrix'
-
-            size = node.right.size
-            if isinstance(node.right, AST.MatrixFuncNode):
-                node.left.size = size
-                print(size)
-            else:
-                #from here on i dont know if it works
-                for row in node.right.values.values:
-                    if isinstance(row, AST.StringOfIntsNode):
-                        row_sizes.append(len(row.ints))
-
-                # Sprawdzenie poprawności inicjalizacji macierzy
-                row_len = -1
-                for row in node.right.values.values:
-                    if row_len == -1:
-                        row_len = len(row.ints)
-                    elif len(row.ints) != row_len:
-                        self.new_error(node.lineno, "Incorrect size of matrix rows!")
-                        return
-
-        elif isinstance(node.right, AST.Bin_RelExprNode):
-            var_type = type
-
-        elif isinstance(node.right, AST.IntNum):
-            var_type = "int"
-
-        elif isinstance(node.right, AST.FloatNum):
-            var_type = "float"
-
-        elif isinstance(node.right, AST.StringNode):
-            var_type = "string"
+            node.left.size = node.right.size            
 
 
         if var_type is None:
@@ -259,8 +174,43 @@ class TypeChecker(NodeVisitor):
         return "matrix"
 
     def visit_ValueListNode(self, node):
-        node.size = len(node.values)
-        self.visit(node.values)
+        same_rows_size = True  # Zakładamy, że wszystkie wiersze są tej samej wielkości
+        matrix_size = None  # Zmienna do przechowywania wymiarów macierzy
+        for value in node.values:
+            value_type = self.visit(value)  # Sprawdzamy typ wartości
+            if isinstance(value, AST.MatrixFuncNode):
+                size = value.size
+                
+                rows = size[0]
+                cols = size[1]
+
+                if matrix_size is None:
+                    matrix_size = (rows, cols)
+                elif matrix_size != (rows, cols):
+                    self.new_error(node.lineno, f"Matrix dimensions mismatch in value list at line {node.lineno}")
+                    same_rows_size = False
+            elif isinstance(value, AST.ValueListNode):
+                rows = len(value.values)
+                cols = len(value.values[0].values) if isinstance(value.values, AST.ValueListNode) else 1 # Zakładając, że pierwsza 'wiersz' nie jest pusty
+                print(matrix_size, rows, cols)
+                if matrix_size is None:
+                    matrix_size = (rows, cols)
+                elif matrix_size != (rows, cols):
+                    self.new_error(node.lineno, f"Matrix dimensions mismatch in value list at line {node.lineno}")
+                    same_rows_size = False
+            else:
+                if matrix_size is None:
+                    matrix_size = 0
+                elif matrix_size != 0:
+                    self.new_error(node.lineno, f"Matrix dimensions mismatch in value list at line {node.lineno}")
+                    same_rows_size = False
+
+        if not same_rows_size:
+            self.new_error(node.lineno, f"Matrix dimensions mismatch in value list at line {node.lineno}")
+            return None
+        node.size = (len(node.values), matrix_size[0], matrix_size[1]) if matrix_size else (1, len(node.values))
+        return 'matrix'
+        
 
     def visit_MatrixRowsNode(self, node):
         for value in node.values:
@@ -387,7 +337,6 @@ class TypeChecker(NodeVisitor):
                 print(f"{scope.name.upper()} -> name: {name}, type: {symbol.type}")
 
     def evaluate_constant_expression(self, node):
-        """Prosta metoda do obliczania wartości wyrażeń stałych."""
         if isinstance(node, AST.IntNum):
             return node.value
         elif isinstance(node, AST.Bin_RelExprNode):
